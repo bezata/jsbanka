c823619a2a88445d77374f69ca8531824c2b020c
bezat <61169260+bezata@users.noreply.github.com>
2022-05-08 20:17:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ const displayMovements = function (movements, sort = false) {
   const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
 
   movs.forEach(function (mov, i) {
-    const type = mov > 0 ? 'yatırma' : 'çekme';
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
       <div class="movements__row">
